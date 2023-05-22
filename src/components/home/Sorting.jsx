290805c8d8986e22d6bd45c672a43f98c45b3312
sarpergulfirat <sarpergulfirat@gmail.com>
2023-05-22 15:23:25 +0300

    category and product added , fetch api , redux toolkit completed

--- src/components/home/Sorting.jsx
+++ src/components/home/Sorting.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Sorting = () => {
   return (
-    <div className='bg-gray-100 my-5 p-5'>
-      Sorting
+    <div className='bg-gray-100 my-5 p-5 flex items-center justify-end'>
+        <select className='bg-white-200 py-3 px-5' name="" id="">
+            <option value="">SEÇİNİZ</option>
+            <option value="inc">Artan</option>
+            <option value="dec">Azalan</option>
+        </select>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import Category from '../components/home/Category'
 import Products from '../components/home/Products'
 import SliderComp from '../components/home/SliderComp'
 import Sorting from '../components/home/Sorting'
 
 const Home = () => {
+    const [sort, setSort] = useState('');
+    const [category, setCategory] = useState('');
+
     return (
         <div>
             <SliderComp />
